225eca6e0e056b0ed110129916e81c539179d223
Ilya Zub <282605+ilyazub@users.noreply.github.com>
2022-02-23 22:24:58 +0200

    Add styles for search results

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import styles from "./SearchResult.module.css";
 
 export function SearchResult(result) {
   return (
-    <li key={result.search_metadata.id}>
-      {result.search_metadata.id} &rarr; {result.search_parameters.q}
+    <li key={result.search_metadata.id} className={styles.result}>
+      <p>
+        Search ID:{" "}
+        <a href={result.search_metadata.json_endpoint}>
+          {result.search_metadata.id}
+        </a>
+      </p>
+      <p>Query: {result.search_parameters.q}</p>
+      <p>Results: {result.organic_results.length}</p>
+      <p>Keys: {Object.keys(result).join("\n")}</p>
     </li>
   );
 }
--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import { SearchResult } from "./SearchResult";
 
+import PropTypes from "prop-types";
+import styles from "./SearchResults.module.css";
+
 export function SearchResults({ results, isLoading, error }) {
   if (isLoading)
-    return <p>Loading...</p>;
+    return (
+      <p>
+        Loading <span className={styles.loading}></span>
+      </p>
+    );
 
   if (!results || results.length === 0) {
-    return <p>Click "Search" &uarr; to continue.</p>;
+    return <p>Click &quot;Search&quot; &uarr; to continue.</p>;
   }
 
-  if (error)
-    return <p>Error: {error}</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <section>
-      <span>Search results:</span>
-      <ul>{results.map(SearchResult)}</ul>
+      <h3>Search results ({results.length})</h3>
+      <ul className={styles.resultsContainer}>{results.map(SearchResult)}</ul>
     </section>
   );
 }
+
+SearchResults.propTypes = {
+  results: PropTypes.array,
+  isLoading: PropTypes.bool,
+  error: PropTypes.object,
+};
